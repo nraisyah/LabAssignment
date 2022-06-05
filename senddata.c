f22a2bb6c43865b4252d58d3d9dffd7ed434ad6f
nraisyah <2020828426@student.uitm.edu.my>
2022-06-05 16:10:16 +0000

    lab 4: receiving data from remote server (modified)

--- senddata.c
+++ senddata.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-#include <string.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>	//inet_addr
+#include<string.h>
+#include <unistd.h>
 
 int main(int argc , char *argv[])
 {
 	int socket_desc;
-	struct sockaddr_in server; 
-              char *message;
+	struct sockaddr_in server;
+	char *message , server_reply[2000];
 	
 	//Create socket
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -37,5 +38,14 @@ int main(int argc , char *argv[])
 		return 1;
 	}
 	puts("Data Send\n");
+                    //Receive a reply from the server
+	if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+	{
+		puts("recv failed");
+	}
+	puts("Reply received\n");
+	puts(server_reply);
+	close(socket_desc);
 	return 0;
 }
+
